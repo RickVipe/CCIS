b2280846f3cc351268e3924aedc070b0dc4fd2bd
Rayo01 <danilo.ud6@gmail.com>
2018-01-27 16:28:59 -0500

    configuracion para el migrate

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-
+        /*
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
@@ -53,12 +53,12 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
-        /*'mysql' => [
+        */
+        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'dbinmaculadaconcepcion'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -67,7 +67,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],*/
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
