285dcd0a413ad34be929a6e0d675ff24a9e895a8
Rayo01 <danilo.ud6@gmail.com>
2018-01-27 12:46:23 -0500

    como estaba inicialmente

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('layouts.layout');
-});
-
-/*Route::get('/', function () {
     return view('welcome');
-});*/
+});
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
