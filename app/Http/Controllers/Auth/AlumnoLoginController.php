3173bf021beeffc759bf422742c249b04fa9031b
Alexander Estrada <alexanderstrada96@gmail.com>
2018-01-28 23:20:39 -0500

    correcion ruta alumnos.index

--- app/Http/Controllers/Auth/AlumnoLoginController.php
+++ app/Http/Controllers/Auth/AlumnoLoginController.php
@@ -28,7 +28,7 @@ class AlumnoLoginController extends Controller
     // Attempt to log the user in
     if (Auth::guard('alumno')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
       // if successful, then redirect to their intended location
-      return redirect()->intended(route('alumno.index'));
+      return redirect()->intended(route('alumnos.index'));
     }
 
     // if unsuccessful, then redirect back to the login with the form data
